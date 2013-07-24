6626e858f2ddb576a8c43b6ec1b81ad2cba5d245
WanWizard <wanwizard@wanwizard.eu>
2013-07-24 16:23:07 +0200

    added bootstrap support; added ServicesProvider

---
+++ src/Fuel/Display/ServicesProvider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package    Fuel\Display
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Display;
+
+use Fuel\Dependency\ServiceProvider;
+use Fuel\FileSystem\Finder;
+
+/**
+ * ServicesProvider class
+ *
+ * Defines the services published by this namespace to the DiC
+ *
+ * @package  Fuel\Display
+ *
+ * @since  1.0.0
+ */
+class ServicesProvider extends ServiceProvider
+{
+	/**
+	 * @var  array  list of service names provided by this provider
+	 */
+	public $provides = array('display.manager');
+
+	/**
+	 * Service provider definitions
+	 */
+	public function provide()
+	{
+		// \Fuel\Display\ViewManager
+		$this->register('display.manager', function ($dic, Finder $finder, array $config = array())
+		{
+			return new ViewManager($finder, $config);
+		});
+	}
+}
