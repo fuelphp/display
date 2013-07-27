f010d7c276d43ca8ac6909ff9c44c95feb7973b0
WanWizard <wanwizard@wanwizard.eu>
2013-07-27 22:12:06 +0200

    added view parsers to the Display service provider

--- src/Fuel/Display/ServicesProvider.php
+++ src/Fuel/Display/ServicesProvider.php
@@ -27,7 +27,7 @@ class ServicesProvider extends ServiceProvider
 	/**
 	 * @var  array  list of service names provided by this provider
 	 */
-	public $provides = array('view', 'parser.php');
+	public $provides = array('viewmanager', 'parser.php', 'parser.markdown', 'parser.mustache', 'parser.twig', 'parser.smarty');
 
 	/**
 	 * Service provider definitions
@@ -35,7 +35,7 @@ class ServicesProvider extends ServiceProvider
 	public function provide()
 	{
 		// \Fuel\Display\ViewManager
-		$this->register('view', function ($dic, Finder $finder, array $config = array())
+		$this->register('viewmanager', function ($dic, Finder $finder, array $config = array())
 		{
 			return new ViewManager($finder, $config);
 		});
@@ -44,6 +44,29 @@ class ServicesProvider extends ServiceProvider
 		$this->register('parser.php', function ($dic)
 		{
 			return new Parser\Php();
+		});
+
+		// \Fuel\Display\Parser\Markdown
+		$this->register('parser.markdown', function ($dic)
+		{
+			return new Parser\Markdown();
+		});
+		// \Fuel\Display\Parser\Mustache
+		$this->register('parser.mustache', function ($dic)
+		{
+			return new Parser\MustacheLoader();
+		});
+
+		// \Fuel\Display\Parser\Twig
+		$this->register('parser.tiwg', function ($dic)
+		{
+			return new Parser\TwigLoader();
+		});
+
+		// \Fuel\Display\Parser\Smarty
+		$this->register('parser.smarty', function ($dic)
+		{
+			return new Parser\Smarty();
 		});
 	}
 }
