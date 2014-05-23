fa731a93caae0fed6139399ba09d12cac9bd52aa
stevewest <uruwolf@gmail.com>
2014-05-23 08:52:19 +0100

    Drops in codeception

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
+
 include 'vendor/autoload.php';
 
-use Fuel\FileSystem\Finder;
-use Fuel\Presenter\ViewManager;
-use Fuel\Presenter\Parser\Php as PhpParser;
-use Fuel\Presenter\Parser\Twig as TwigParser;
-use Fuel\Presenter\Parser\Smarty as SmartyParser;
-use Fuel\Presenter\Parser\Mustache as MustacheParser;
-use Fuel\Presenter\Parser\Markdown as MarkdownParser;
-use Fuel\Presenter\Parser\MarkdownExtra as MarkdownExtraParser;
+use Fuel\Display\ViewManager;
+use Fuel\Display\Parser\Php as PhpParser;
+use Fuel\Display\Parser\Twig as TwigParser;
+use Fuel\Display\Parser\Smarty as SmartyParser;
+use Fuel\Display\Parser\Mustache as MustacheParser;
+use Fuel\Display\Parser\Markdown as MarkdownParser;
+use Fuel\Display\Parser\MarkdownExtra as MarkdownExtraParser;
 
 $manager = new ViewManager(null, array(
 	'cache' => __DIR__.'/cache',
 ));
 $finder = $manager->getFinder();
 $manager->set('global', 'message');
-$finder->addPath(__dir__);
+$finder->addPath(__DIR__);
+
 $manager->registerParser('php', new PhpParser);
 $manager->registerParser('twig', new TwigParser);
 $manager->registerParser('mustache', new MustacheParser);
 $manager->registerParser('md', new MarkdownParser);
 $manager->registerParser('mde', new MarkdownExtraParser);
 $manager->registerParser('tpl', new SmartyParser);
+
 echo ($manager->forge('test.twig', array('name' => 'Frank'))->render());
 echo $manager->forge('test.mustache', array('name' => 'Mr. Mustache!'));
 echo $manager->forge('test.md')->render();
