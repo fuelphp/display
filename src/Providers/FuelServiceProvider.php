76d577f67d5091b3aee91d0a70ed4064ee6fd9ce
WanWizard <wanwizard@wanwizard.eu>
2014-10-29 00:09:57 +0100

    add an extension to inject the current ViewManager into Parser instances

--- src/Providers/FuelServiceProvider.php
+++ src/Providers/FuelServiceProvider.php
@@ -41,6 +41,42 @@ class FuelServiceProvider extends ServiceProvider
 	 */
 	public function provide()
 	{
+		/**
+		 * Define the generic extensions provided by this service provider
+		 */
+		$this->extension('getViewManagerInstance', function($container, $instance)
+		{
+			try
+			{
+				$stack = $container->resolve('requeststack');
+				if ($request = $stack->top())
+				{
+					$app = $request->getComponent()->getApplication();
+				}
+				else
+				{
+					$app = $container->resolve('application::__main');
+				}
+
+				if (is_callable(array($instance, 'setViewManager')))
+				{
+					$instance->setViewManager($app->getViewManager());
+				}
+				elseif (is_callable(array($instance, 'setManager')))
+				{
+					$instance->setManager($app->getViewManager());
+				}
+				else
+				{
+					$instance->viewmanager = $app->getViewManager();
+				}
+			}
+			catch (\Fuel\Dependency\ResolveException $e)
+			{
+				// ignore
+			}
+		});
+
 		// \Fuel\Display\ViewManager
 		$this->register('viewmanager', function ($dic, Finder $finder, array $config = [])
 		{
@@ -52,41 +88,48 @@ class FuelServiceProvider extends ServiceProvider
 		{
 			return $dic->resolve('Fuel\Display\Parser\Php');
 		});
+		$this->extend('parser.php', 'getViewManagerInstance');
 
 		// \Fuel\Display\Parser\Markdown
 		$this->register('parser.markdown', function ($dic)
 		{
 			return $dic->resolve('Fuel\Display\Parser\Markdown');
 		});
+		$this->extend('parser.markdown', 'getViewManagerInstance');
 
 		// \Fuel\Display\Parser\Mustache
 		$this->register('parser.mustache', function ($dic)
 		{
 			return $dic->resolve('Fuel\Display\Parser\Mustache');
 		});
+		$this->extend('parser.mustache', 'getViewManagerInstance');
 
 		// \Fuel\Display\Parser\Twig
 		$this->register('parser.twig', function ($dic)
 		{
 			return $dic->resolve('Fuel\Display\Parser\Twig');
 		});
+		$this->extend('parser.twig', 'getViewManagerInstance');
 
 		// \Fuel\Display\Parser\Smarty
 		$this->register('parser.smarty', function ($dic)
 		{
 			return $dic->resolve('Fuel\Display\Parser\Smarty');
 		});
+		$this->extend('parser.smarty', 'getViewManagerInstance');
 
 		// \Fuel\Display\Parser\Handlebars
 		$this->register('parser.handlebars', function ($dic)
 		{
 			return $dic->resolve('Fuel\Display\Parser\Handlebars');
 		});
+		$this->extend('parser.handlebars', 'getViewManagerInstance');
 
 		// \Fuel\Display\Parser\Handlebars - alternate .hbs
 		$this->register('parser.hbs', function ($dic)
 		{
 			return $dic->resolve('Fuel\Display\Parser\Handlebars');
 		});
+		$this->extend('parser.hbs', 'getViewManagerInstance');
 	}
 }
